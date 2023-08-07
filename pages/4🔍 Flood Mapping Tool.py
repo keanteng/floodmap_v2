0ddec2fabce5d8ad3823ca0af3adc4d53b2ed552
Khor Kean Teng <u2004763@siswa.um.edu.my>
2023-08-07 10:14:40 +0800

    update

--- pages/4🔍 Flood Mapping Tool.py
+++ pages/4🔍 Flood Mapping Tool.py
@@ -1044,6 +1044,6 @@ with st.expander("Further Analysis", expanded=False):
     with st.echo():
         m = st.session_state.Map2
         cities = pd.read_csv("analytics/data2/all_states_all_years_geocoded.csv")
-        m.add_points_from_xy(cities, x="longitude", y="latitude")
+        m.add_points_from_xy(cities, x="Longitude", y="Latitude")
         
         m.to_streamlit(height = 700)
